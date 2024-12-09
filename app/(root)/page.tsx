ebfed087f69437c95685b4ef0f4cfcf347978bb7
Davidson Martins <davidsoncarrilho@hotmail.com>
2024-12-09 16:43:59 +0100

    Live posts updating

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,7 @@
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({
   searchParams,
@@ -7,6 +10,9 @@ export default async function Home({
 }) {
   // for URL Server rendering Search form
   const query = (await searchParams).query;
+
+  // To revalidate data in live when a new post is created
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   return (
     <>
@@ -19,6 +25,22 @@ export default async function Home({
           Competitions.
         </p>
         <SearchForm query={query} />
+        <section className="section_container">
+          <p className="text-30-semibold">
+            {query ? `Search results for ${query}` : "All startups"}
+          </p>
+          <ul className="mt-7 card_grid">
+            {posts?.length > 0 ? (
+              posts.map((post: StartupTypeCard) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+            ) : (
+              <p className="no-results">No startups found</p>
+            )}
+          </ul>
+        </section>
+
+        <SanityLive />
       </section>
     </>
   );
