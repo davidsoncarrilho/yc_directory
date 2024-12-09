9b6ae66064dcc84f3178935e8878d922a795a1c9
Davidson Martins <davidsoncarrilho@hotmail.com>
2024-12-09 19:07:16 +0100

    PPR views

--- components/View.tsx
+++ components/View.tsx
@@ -1,7 +1,36 @@
 // import Ping from "@/components/Ping";
 
+import { STARTUP_VIEWS_QUERY } from "@/lib/queries";
+import { client } from "@/sanity/lib/client";
+import Ping from "./Ping";
+import { writeClient } from "@/sanity/lib/write-client";
+
+import { unstable_after as after } from "next/server";
+
 async function View({ id }: { id: string }) {
-  return <div className="view-container">VIEW</div>;
+  const { views: totalViews } = await client
+    .withConfig({ useCdn: false })
+    .fetch(STARTUP_VIEWS_QUERY, { id });
+
+  after(
+    async () =>
+      await writeClient
+        .patch(id)
+        .set({ views: totalViews + 1 })
+        .commit()
+  );
+
+  return (
+    <div className="view-container">
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
+
+      <p className="view-text">
+        <span className="font-black">Views: {totalViews}</span>
+      </p>
+    </div>
+  );
 }
 
 export default View;
